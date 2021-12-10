90f0b0f9645e78740550f8c88d03cf5e1155ec2e
genndy007 <kochevgenazp@gmail.com>
2021-12-10 18:53:31 +0200

    Add download audio button

--- frontend/MainWindow.py
+++ frontend/MainWindow.py
@@ -2,57 +2,61 @@ from PyQt5.QtWidgets import (
     QMainWindow, QLineEdit, QLabel, QPushButton, QWidget,
     QGridLayout, QFileDialog
 )
-from backend.Downloader import download_video
-# from .ElementsParameters import (
-#     COORDS_PROMPT_URL,
-#     COORDS_PROMPT_TEXT,
-#     COORDS_CONFIRM_BUTTON,
-#
-#     SIZE_PROMPT_URL,
-# )
+from backend.Downloader import download_video, download_audio
+
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
-
-        self.setMinimumSize(200, 200)
+        # Window settings
+        self.setMinimumSize(400, 200)
         self.setWindowTitle("Downloader from YouTube")
-
+        # Creating layout
         central_widget = QWidget(self)
         self.setCentralWidget(central_widget)
-
         grid_layout = QGridLayout(self)
         central_widget.setLayout(grid_layout)
-
+        # Line 1
         self.prompt_text = QLabel(self)
         self.prompt_text.setText('Please paste your link:')
 
         self.url_prompt = QLineEdit(self)
-
-        self.confirm_button = QPushButton('Confirm', self)
-        self.confirm_button.clicked.connect(self.confirm_button_action)
+        # Line 2
+        self.directory_text = QLineEdit(self)
 
         self.choose_directory_button = QPushButton('Choose directory...', self)
         self.choose_directory_button.clicked.connect(self.choose_directory_button_action)
 
-        self.directory_text = QLineEdit(self)
+        # Line 3
+        self.download_video_button = QPushButton('Download video', self)
+        self.download_video_button.clicked.connect(self.download_video_button_action)
 
-        self.result_text = QLabel(self)
-        self.result_text.setText('Status: ')
+        self.download_audio_button = QPushButton('Download audio', self)
+        self.download_audio_button.clicked.connect(self.download_audio_button_action)
 
         self.reset_button = QPushButton('Reset', self)
         self.reset_button.clicked.connect(self.reset_button_action)
 
+        # Line 4
+        self.result_text = QLabel(self)
+        self.result_text.setText('Status: ')
 
-
+        # Show line 1
         grid_layout.addWidget(self.prompt_text, 0, 0)
         grid_layout.addWidget(self.url_prompt, 0, 1)
-        grid_layout.addWidget(self.confirm_button, 0, 2)
+        # Show line 2
         grid_layout.addWidget(self.directory_text, 1, 0)
         grid_layout.addWidget(self.choose_directory_button, 1, 1)
-        grid_layout.addWidget(self.reset_button, 1, 2)
-        grid_layout.addWidget(self.result_text, 2, 0)
+        # Show line 3
+        grid_layout.addWidget(self.reset_button, 2, 0)
+        grid_layout.addWidget(self.result_text, 2, 1)
+        # Show line 4
+        grid_layout.addWidget(self.download_video_button, 3, 0)
+        grid_layout.addWidget(self.download_audio_button, 3, 1)
+
+
+
 
 
     def choose_directory_button_action(self):
@@ -60,11 +64,18 @@ class MainWindow(QMainWindow):
         self.directory_text.setText(directory)
 
 
-    def confirm_button_action(self):
+    def download_video_button_action(self):
         # there could be some calling backend
         directory = self.directory_text.text()
         url = self.url_prompt.text()
         result = download_video(url, directory=directory)
+        self.result_text.setText('Status: Downloaded')
+
+
+    def download_audio_button_action(self):
+        directory = self.directory_text.text()
+        url = self.url_prompt.text()
+        result = download_audio(url, directory)
         self.result_text.setText('Status: Downloaded')
 
     def reset_button_action(self):
