b027249062de1f91a7429ac50af6315333cd72af
genndy007 <kochevgenazp@gmail.com>
2021-12-10 19:01:45 +0200

    Add icon path to main

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import sys
 from PyQt5 import QtWidgets
 from frontend.MainWindow import MainWindow
 
+icon_path = 'static/logo.png'
+
 app = QtWidgets.QApplication(sys.argv)
-main_window = MainWindow()
+main_window = MainWindow(icon_path)
 main_window.show()
 
 sys.exit(app.exec())
